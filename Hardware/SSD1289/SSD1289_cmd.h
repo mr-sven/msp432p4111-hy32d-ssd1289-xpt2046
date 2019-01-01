39680ae3a1e604f05bcd63138eea01a408406e70
Sven <sven.fabricius@livediesel.de>
2019-01-01 18:32:16 +0100

    init squence

--- Hardware/SSD1289/SSD1289_cmd.h
+++ Hardware/SSD1289/SSD1289_cmd.h
@@ -40,9 +40,29 @@
 #define SSD1289_PWR_CTRL_1_AP(n)			(((n) & 7) << 1)
 
 #define SSD1289_COMP_REG_1					0x0005
+#define SSD1289_COMP_REG_1_CPR(n)			(((n) & 0x3F) << 10)
+#define SSD1289_COMP_REG_1_CPG(n)			(((n) & 0x3F) << 2)
+
 #define SSD1289_COMP_REG_2					0x0006
+#define SSD1289_COMP_REG_2_CPB(n)			(((n) & 0x3F) << 2)
+
 #define SSD1289_DISP_CTRL					0x0007
+#define SSD1289_DISP_CTRL_PT(n)				(((n) & 3) << 11)
+#define SSD1289_DISP_CTRL_VLE(n)			(((n) & 3) << 9)
+#define SSD1289_DISP_CTRL_SPT				BIT(8)
+#define SSD1289_DISP_CTRL_GON				BIT(5)
+#define SSD1289_DISP_CTRL_DTE				BIT(4)
+#define SSD1289_DISP_CTRL_CM				BIT(3)
+#define SSD1289_DISP_CTRL_D(n)				((n) & 3)
+
 #define SSD1289_FRM_CYL_CTRL				0x000B
+#define SSD1289_FRM_CYL_CTRL_NO(n)			(((n) & 3) << 14)
+#define SSD1289_FRM_CYL_CTRL_SDT(n)			(((n) & 3) << 12)
+#define SSD1289_FRM_CYL_CTRL_EQ(n)			(((n) & 7) << 8)
+#define SSD1289_FRM_CYL_CTRL_DIV(n)			(((n) & 3) << 6)
+#define SSD1289_FRM_CYL_CTRL_SDIV			BIT(5)
+#define SSD1289_FRM_CYL_CTRL_SRTN			BIT(4)
+#define SSD1289_FRM_CYL_CTRL_RTN(n)			((n) & 0xF)
 
 #define SSD1289_PWR_CTRL_2					0x000C
 #define SSD1289_PWR_CTRL_2_VRC(n)			((n) & 7)
@@ -55,11 +75,32 @@
 #define SSD1289_PWR_CTRL_4_VDV(n)			(((n) & 0x1F) << 8)
 
 #define SSD1289_GAT_SCN_STRT_POS			0x000F
+#define SSD1289_GAT_SCN_STRT_POS_SCN(n)		((n) & 0x1FF)
+
 #define SSD1289_SLEEP_MODE					0x0010
+#define SSD1289_SLEEP_MODE_SLP				BIT(0)
+
 #define SSD1289_ENTRY_MODE					0x0011
+#define SSD1289_ENTRY_MODE_VSMODE			BIT(15)
+#define SSD1289_ENTRY_MODE_DFM(n)			(((n) & 3) << 13)
+#define SSD1289_ENTRY_MODE_TRANS			BIT(12)
+#define SSD1289_ENTRY_MODE_OEDEF			BIT(11)
+#define SSD1289_ENTRY_MODE_WMODE			BIT(10)
+#define SSD1289_ENTRY_MODE_DMODE(n)			(((n) & 3) << 8)
+#define SSD1289_ENTRY_MODE_TY(n)			(((n) & 3) << 6)
+#define SSD1289_ENTRY_MODE_ID(n)			(((n) & 3) << 4)
+#define SSD1289_ENTRY_MODE_AM				BIT(3)
+#define SSD1289_ENTRY_MODE_LG(n)			((n) & 7)
+
 #define SSD1289_GEN_INTF_CTRL				0x0015
+
 #define SSD1289_HORIZ_PORCH					0x0016
+#define SSD1289_HORIZ_PORCH_XL(n)			(((n) & 0xFF) << 8)
+#define SSD1289_HORIZ_PORCH_HBP(n)			((n) & 0xFF)
+
 #define SSD1289_VERT_PORCH					0x0017
+#define SSD1289_VERT_PORCH_VFP(n)			(((n) & 0x7F) << 8)
+#define SSD1289_VERT_PORCH_VBP(n)			((n) & 0xFF)
 
 #define SSD1289_PWR_CTRL_5					0x001E
 #define SSD1289_PWR_CTRL_5_NOTP				BIT(7)
@@ -81,13 +122,29 @@
 #define SSD1289_GAMMA_CTRL_8			0x0037
 #define SSD1289_GAMMA_CTRL_9			0x003A
 #define SSD1289_GAMMA_CTRL_10			0x003B
+
 #define SSD1289_VERT_SCROLL_1			0x0041
+#define SSD1289_VERT_SCROLL_1_VL1(n)	((n) & 0xFF)
+
 #define SSD1289_VERT_SCROLL_2			0x0042
+#define SSD1289_VERT_SCROLL_2_VL2(n)	((n) & 0xFF)
+
 #define SSD1289_HORIZ_RAM_ADDR_POS		0x0044
+#define SSD1289_HORIZ_RAM_ADDR_POS_HSE(n)	(((n) & 0xFF) << 8)
+#define SSD1289_HORIZ_RAM_ADDR_POS_HSA(n)	((n) & 0xFF)
+
 #define SSD1289_VERT_RAM_ADDR_STRT_POS	0x0045
+#define SSD1289_VERT_RAM_ADDR_STRT_POS_VSA(n)	((n) & 0xFF)
+
 #define SSD1289_VERT_RAM_ADDR_END_POS	0x0046
+#define SSD1289_VERT_RAM_ADDR_END_POS_VEA(n)	((n) & 0xFF)
+
 #define SSD1289_FIRST_WND_START			0x0048
+#define SSD1289_FIRST_WND_START_SS(n)	((n) & 0xFF)
+
 #define SSD1289_FIRST_WND_END			0x0049
+#define SSD1289_FIRST_WND_END_SE(n)		((n) & 0xFF)
+
 #define SSD1289_SND_WND_START			0x004A
 #define SSD1289_SND_WND_END				0x004B
 #define SSD1289_SET_GDDRAM_X_ADDR_CNT	0x004E
