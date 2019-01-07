ae5029266cfaa9ba0b4b49f7f134410f0d80a86e
Sven <sven.fabricius@livediesel.de>
2019-01-07 19:04:31 +0100

    defined hardware pins for XPT

--- Board.h
+++ Board.h
@@ -13,22 +13,30 @@
 #define BOARD_H_
 
 // GLUE
-#define PASTER2(x,y)         x ## y
-#define EVALUATOR2(x,y)      PASTER2(x,y)
-#define PASTER3(x,y,z)       x ## y ## z
-#define EVALUATOR3(x,y,z)    PASTER3(x,y,z)
+#define PASTER2(x,y)		x ## y
+#define EVALUATOR2(x,y)		PASTER2(x,y)
+#define PASTER3(x,y,z)		x ## y ## z
+#define EVALUATOR3(x,y,z)	PASTER3(x,y,z)
 
-#define PWM_PERIOD           (CS_48MHZ/5000)
-#define LED_PWM              1.0 // 0.3
+#define PWM_PERIOD			(CS_48MHZ/5000)
+#define LED_PWM				1.0 // 0.3
 
 #define JTAG_ENABLED
 
-#define LCD_DATA             PB
-#define LCD_CTRL             PC
-#define LCD_CS               GPIO_PIN4
-#define LCD_RS               GPIO_PIN1
-#define LCD_RD               GPIO_PIN3
-#define LCD_WR               GPIO_PIN2
-#define LCD_RESET            GPIO_PIN0
+#define LCD_DATA			PB
+#define LCD_CTRL			PC
+#define LCD_RESET			GPIO_PIN0
+#define LCD_RS				GPIO_PIN1
+#define LCD_WR				GPIO_PIN2
+#define LCD_RD				GPIO_PIN3
+#define LCD_CS				GPIO_PIN4
+
+#define TOUCH_SPI			EUSCI_B1_SPI
+#define TOUCH_PORT			PC
+#define TOUCH_IRQ			GPIO_PIN9
+#define TOUCH_CS			GPIO_PIN10
+#define TOUCH_SCK			GPIO_PIN11
+#define TOUCH_SI			GPIO_PIN12
+#define TOUCH_SO			GPIO_PIN13
 
 #endif /* BOARD_H_ */
